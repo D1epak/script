933f1dee2e44984afb2d0ec62846f69cf9b8b223
Evgeny Korobkov <112511598+D1epak@users.noreply.github.com>
2022-12-06 19:36:25 +0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,12 @@
 # Обновляем систему и список пакетов
-sudo apt-get update -y
+apt-get update -y
 # Устанавливаем нужную для выполнения скрипта софтину
-sudo apt-get install \
+apt-get install \
     ca-certificates \
     curl \
     gnupg \
     lsb-release -y
 # Устанавливаем докер и все нужные для него приколы
-sudo apt-get install docker docker-compose -y
+apt-get install docker docker-compose -y
 git clone https://github.com/D1epak/server.git
 cd server && docker compose up -d --build
